0ea55c25b7d6fa7683c3ddc599ac32c5e50ed36f
paulphoku <paulphoku@gmail.com>
2020-03-29 12:16:35 +0200

    appbar and options menu implemented

--- app/src/main/java/com/paulphoku/ems/HomeActivity.java
+++ app/src/main/java/com/paulphoku/ems/HomeActivity.java
@@ -1,12 +1,21 @@
 package com.paulphoku.ems;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
+//import androidx.appcompat.widget.Toolbar;
 import androidx.viewpager.widget.PagerAdapter;
 import androidx.viewpager.widget.ViewPager;
 
+
+import android.content.Intent;
 import android.os.Bundle;
-import android.widget.TableLayout;
-import android.widget.TableRow;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
+import android.widget.Toolbar;
 
 import com.google.android.material.tabs.TabItem;
 import com.google.android.material.tabs.TabLayout;
@@ -16,32 +25,53 @@ public class HomeActivity extends AppCompatActivity {
     private TabLayout tabLayout;
     private ViewPager viewPager;
     private TabItem tab1, tab2, tab3;
-    public PagerAdapter pageadapter;
+    public PagerAdapter pagerAdapter;
 
+    private Toolbar toolbar;
+
+
+
+    //Exit
+    public void exit() {
+        moveTaskToBack(true);
+        android.os.Process.killProcess(android.os.Process.myPid());
+        System.exit(1);
+    }
+    //go to Login
+    public void launchOtpActivity(View view) {
+        Intent intent = new Intent(this, LoginActivity.class);
+        startActivity(intent);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
-        tabLayout = (TabLayout) findViewById(R.id.tablayout);
+        tabLayout = (TabLayout) findViewById(R.id.tabLayout);
         tab1 = (TabItem) findViewById(R.id.Tab1);
         tab2 = (TabItem) findViewById(R.id.Tab2);
         tab3 = (TabItem) findViewById(R.id.Tab3);
         viewPager = findViewById(R.id.viewpager);
 
-        pageadapter = new PageAdapter(getSupportFragmentManager(), tabLayout.getTabCount() );
-        viewPager.setAdapter(pageadapter);
+        toolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().setTitle("Main Page");
+        }
+        toolbar.inflateMenu(R.menu.main_menu);
+
+        pagerAdapter = new PageAdapter(getSupportFragmentManager(), tabLayout.getTabCount() );
+        viewPager.setAdapter(pagerAdapter);
         tabLayout.setOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
             @Override
             public void onTabSelected(TabLayout.Tab tab) {
                 viewPager.setCurrentItem(tab.getPosition());
                 if(tab.getPosition() == 0){
-                    pageadapter.notifyDataSetChanged();
+                    pagerAdapter.notifyDataSetChanged();
                 }else if(tab.getPosition() == 1) {
-                    pageadapter.notifyDataSetChanged();
+                    pagerAdapter.notifyDataSetChanged();
                 }else if(tab.getPosition() == 2) {
-                    pageadapter.notifyDataSetChanged();
+                    pagerAdapter.notifyDataSetChanged();
                 }
             }
 
@@ -57,5 +87,36 @@ public class HomeActivity extends AppCompatActivity {
         });
 
        viewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
+    }
+
+    //options menu method
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return true;
+    }
+    //options menu on select TODO
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch (item.getItemId())
+        {
+            case R.id.settings:
+                Toast.makeText(this, "Settings", Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.exit:
+                Toast.makeText(this, "Exit", Toast.LENGTH_SHORT).show();
+                //exit();
+                return true;
+            case R.id.update_profile:
+                Toast.makeText(this, "Update Profile", Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.log:
+                Toast.makeText(this, "Log", Toast.LENGTH_SHORT).show();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
